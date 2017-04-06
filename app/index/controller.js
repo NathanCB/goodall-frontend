34133c435ed0d53141e0f3f83e00d0c3a54b8514
Evan Hickman <travisevanhickman@gmail.com>
2017-04-06 11:40:18 -0500

    searchLocation function

--- app/index/controller.js
+++ app/index/controller.js
@@ -1,4 +1,17 @@
 import Ember from 'ember';
 
+const apiKey = 'AIzaSyByoGefg3HDYycC853DvId8_cmowJgNaAc';
+
 export default Ember.Controller.extend({
+  actions: {
+    searchLocation (query) {
+      fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${query}&key=${apiKey}`)
+        .then(r => r.json())
+        .then((result) => {
+          this.set('lat', result.results[0].geometry.location.lat);
+          this.set('lng', result.results[0].geometry.location.lng);
+          debugger;
+        })
+    }
+  }
 });
