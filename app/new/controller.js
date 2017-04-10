7dc3b8c7c7c7d83d906aeaa96d4e32e787146224
Evan Hickman <travisevanhickman@gmail.com>
2017-04-10 15:25:35 -0500

    install ember file uploader addon

--- app/new/controller.js
+++ app/new/controller.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
+import { validatePresence, validateFormat, validateLength, validateConfirmation } from 'ember-changeset-validations/validators'
 
 export default Ember.Controller.extend({
+  session: Ember.inject.service(),
+
+  validator: {
+    eventImg: [
+      validatePresence(true)
+    ]
+  }
 });
