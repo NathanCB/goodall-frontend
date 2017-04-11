abdb34fca9a16fdc8ec74ccde1c3194ba1ece0d8
Evan Hickman <travisevanhickman@gmail.com>
2017-04-10 20:19:09 -0500

    add date-picker component

---
+++ app/components/date-picker/component.js
@@ -0,0 +1,17 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  actions: {
+    setDateRange() {
+
+    },
+
+    hideDatePicker() {
+
+    },
+
+    cancelDatePicker() {
+
+    }
+  }
+});
