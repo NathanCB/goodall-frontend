498b0412b2c91301c68546ab8c6841ab51e69810
Evan Hickman <travisevanhickman@gmail.com>
2017-04-11 19:39:57 -0500

    work on file uploader

--- app/new/route.js
+++ app/new/route.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.store.peekAll('upload');
+    // return this.store.peekAll('upload');
+    return {};
   }
 });
