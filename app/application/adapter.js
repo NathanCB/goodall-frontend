8efd249fbab80c1049486616e3a9b4dc1d8e7ded
Evan Hickman <travisevanhickman@gmail.com>
2017-04-17 09:19:05 -0500

    manage current user

--- app/application/adapter.js
+++ app/application/adapter.js
@@ -3,4 +3,13 @@ import config from 'goodall-frontend/config/environment';
 
 export default DS.JSONAPIAdapter.extend({
   host: config.DS.host,
+
+  urlForQueryRecord(query) {
+    if (query.me) {
+      delete query.me;
+      return `${this._super(...arguments)}/me`;
+    }
+
+    return this._super(...arguments);
+  }
 });
--- app/application/route.js
+++ app/application/route.js
@@ -1,5 +1,22 @@
 import Ember from 'ember';
 import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
 
+export default Ember.Route.extend(ApplicationRouteMixin, {
+  currentUser: Ember.inject.service(),
+  session: Ember.inject.service(),
 
-export default Ember.Route.extend(ApplicationRouteMixin);
+  beforeModel() {
+    return this._loadCurrentUser();
+  },
+
+  sessionAuthenticated() {
+    this._super(...arguments);
+    this._loadCurrentUser();
+  },
+
+  _loadCurrentUser() {
+    return this.get('currentUser').load().catch(() => {
+      this.get('session').invalidate()
+    });
+  }
+});
