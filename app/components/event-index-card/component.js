aa08c3cfa231711c8382da0dd7511cb0842ba789
Evan Hickman <travisevanhickman@gmail.com>
2017-04-16 21:10:46 -0500

    stop using event-index-card Component

--- app/components/event-index-card/component.js
+++ app/components/event-index-card/component.js
@@ -1,61 +1,61 @@
-import Ember from 'ember';
-
-export default Ember.Component.extend({
-  tagName: '',
-
-  isShowingModal: false,
-  actions: {
-    toggleModal: function() {
-      this.toggleProperty('isShowingModal');
-    }
-  }
-
-  // fonts: [
-  //   'vt',
-  //   'quicksand',
-  //   'raleway',
-  //   'lora',
-  //   'inconsolata',
-  //   'catamaran',
-  //   'crimson',
-  //   'abril',
-  //   'gloria',
-  //   'space',
-  //   'amatic',
-  //   'josefin',
-  //   'righteous',
-  //   'bungee',
-  //   'poiret',
-  //   'courgette',
-  //   'playfair',
-  //   'didact',
-  //   'sorts',
-  // ],
-
-  // fontSizes: [
-  //   '2',
-  //   '3',
-  //   '4',
-  //   '5',
-  // ],
-  //
-  // fontColors: [
-  //   'green',
-  //   'yellow',
-  //   'blue',
-  //   'red',
-  //   'pale-white',
-  // ],
-  //
-  // init() {
-  //   this._super(...arguments);
-  //
-  //   const randomFont = this.fonts[Math.floor(Math.random()*this.fonts.length)];
-  //   const randomFontSize = this.fontSizes[Math.floor(Math.random()*this.fontSizes.length)];
-  //   const randomFontColor = this.fontColors[Math.floor(Math.random()*this.fontColors.length)];
-  //
-  //   this.set('font', randomFont);
-  //   this.set('fontSize', randomFontSize);
-  //   this.set('fontColor', randomFontColor);
-  // }
-});
+// import Ember from 'ember';
+//
+// export default Ember.Component.extend({
+//   tagName: '',
+//
+//   isShowingModal: false,
+//   actions: {
+//     toggleModal: function() {
+//       this.toggleProperty('isShowingModal');
+//     }
+//   }
+//
+//   fonts: [
+//     'vt',
+//     'quicksand',
+//     'raleway',
+//     'lora',
+//     'inconsolata',
+//     'catamaran',
+//     'crimson',
+//     'abril',
+//     'gloria',
+//     'space',
+//     'amatic',
+//     'josefin',
+//     'righteous',
+//     'bungee',
+//     'poiret',
+//     'courgette',
+//     'playfair',
+//     'didact',
+//     'sorts',
+//   ],
+//
+//   fontSizes: [
+//     '2',
+//     '3',
+//     '4',
+//     '5',
+//   ],
+//
+//   fontColors: [
+//     'green',
+//     'yellow',
+//     'blue',
+//     'red',
+//     'pale-white',
+//   ],
+//
+//   init() {
+//     this._super(...arguments);
+//
+//     const randomFont = this.fonts[Math.floor(Math.random()*this.fonts.length)];
+//     const randomFontSize = this.fontSizes[Math.floor(Math.random()*this.fontSizes.length)];
+//     const randomFontColor = this.fontColors[Math.floor(Math.random()*this.fontColors.length)];
+//
+//     this.set('font', randomFont);
+//     this.set('fontSize', randomFontSize);
+//     this.set('fontColor', randomFontColor);
+//   }
+// });
--- app/helpers/event-card-class.js
+++ app/helpers/event-card-class.js
@@ -1,7 +1,7 @@
-import Ember from 'ember';
-
-export function eventCardClass(params, {font, fontSize, fontColor}) {
-  return `index-grid__item f-${font} f-${fontSize} f-${fontColor}`;
-}
-
-export default Ember.Helper.helper(eventCardClass);
+// import Ember from 'ember';
+//
+// export function eventCardClass(params, {font, fontSize, fontColor}) {
+//   return `index-grid__item f-${font} f-${fontSize} f-${fontColor}`;
+// }
+//
+// export default Ember.Helper.helper(eventCardClass);
