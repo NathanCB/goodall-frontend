72af3b2725c85cc5a4cc7e26fbbb9fd457666a16
Evan Hickman <travisevanhickman@gmail.com>
2017-04-21 15:05:39 -0500

    update buttons

--- app/event/detail/controller.js
+++ app/event/detail/controller.js
@@ -4,12 +4,12 @@ import moment from 'moment';
 export default Ember.Controller.extend({
   session: Ember.inject.service('session'),
   currentUser: Ember.inject.service(),
-  
+
   actions: {
     toggleModal: function() {
       this.transitionToRoute('event.index');
     },
-    
+
     deleteEvent() {
       this.model.destroyRecord().then(() => {
         this.transitionToRoute('event');
